d1129bbb4b6dbc650ec897a2b7b794da446b59ff
hikalium <hikalium@hikalium.com>
2017-12-05 21:57:15 +0000

    Impl init (not completed)

--- nvme.cc
+++ nvme.cc
@@ -1,5 +1,64 @@
 #include "nvme.h"
 
+void DevNvme::MapControlRegisters() {
+  uint32_t addr_bkup;
+  _pci.ReadPciReg(DevPci::kBaseAddressReg0, addr_bkup);
+  _pci.WritePciReg(DevPci::kBaseAddressReg0, 0xFFFFFFFF);
+  uint32_t size;
+  _pci.ReadPciReg(DevPci::kBaseAddressReg0, size);
+  size = ~size + 1;
+  _pci.WritePciReg(DevPci::kBaseAddressReg0, addr_bkup);
+
+  int fd = open("/sys/class/uio/uio0/device/resource0", O_RDWR);
+  if (!fd) {
+    perror("MapControlRegisters");
+    exit(EXIT_FAILURE);
+  }
+  _ctrl_reg_8_base = reinterpret_cast<volatile uint8_t *>(
+      mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+  _ctrl_reg_8_size = size;
+  close(fd);
+  //
+  _ctrl_reg_32_base = reinterpret_cast<volatile uint32_t *>(_ctrl_reg_8_base);
+  _ctrl_reg_64_base = reinterpret_cast<volatile uint64_t *>(_ctrl_reg_8_base);
+}
+
+void DevNvme::InitAdminQueues() {
+  assert(sizeof(CommandSet) == 64);
+  assert(sizeof(CompletionQueueEntry) == 16);
+  // sizeof(CompletionQueueEntry) may change
+  // in the future impl (see section 4.6 in spec)
+
+  _mem_for_asq = new Memory(sizeof(CommandSet) * kASQSize);
+  _mem_for_acq = new Memory(sizeof(CompletionQueueEntry) * kACQSize);
+  bzero(_mem_for_acq->GetVirtPtr<void>(),
+        _mem_for_acq->GetSize());  // clear phase tag.
+
+  _ctrl_reg_64_base[kCtrlReg64OffsetASQ] = _mem_for_asq->GetPhysPtr();
+  _ctrl_reg_64_base[kCtrlReg64OffsetACQ] = _mem_for_acq->GetPhysPtr();
+
+  {
+    uint32_t aqa = 0;
+    aqa |= (0xfff & _mem_for_acq->GetSize()) << 16;
+    aqa |= (0xfff & _mem_for_asq->GetSize());
+    _ctrl_reg_32_base[kCtrlReg32OffsetAQA] = aqa;
+  }
+}
+
+void PrintControllerConfiguration(DevNvme::ControllerConfiguration cc) {
+  printf("CC: EN=%d CSS=%d MPS=%d AMS=%d IOSQES=%d IOCQES=%d  \n", cc.bits.EN,
+         cc.bits.CSS, cc.bits.MPS, cc.bits.AMS, cc.bits.IOSQES, cc.bits.IOCQES);
+}
+
+void PrintControllerStatus(DevNvme::ControllerStatus csts) {
+  printf("CSTS: RDY=%d CFS=%d SHST=%d NSSRO=%d PP=%d  \n", csts.bits.RDY,
+         csts.bits.CFS, csts.bits.SHST, csts.bits.NSSRO, csts.bits.PP);
+}
+
+void PrintControllerCapabilities(DevNvme::ControllerCapabilities cap) {
+  printf("CAP: TO=%d NSSRS=%d  \n", cap.bits.TO, cap.bits.NSSRS);
+}
+
 void DevNvme::Init() {
   _pci.Init();
   uint16_t vid, did;
@@ -7,42 +66,87 @@ void DevNvme::Init() {
   _pci.ReadPciReg(DevPci::kDeviceIDReg, did);
   printf("vid:%08X did:%08X\n", vid, did);
 
-  /*
-    uint8_t interface, sub, base;
-    _pci.ReadPciReg(DevPci::kRegInterfaceClassCode, interface);
-    _pci.ReadPciReg(DevPci::kRegSubClassCode, sub);
-    _pci.ReadPciReg(DevPci::kRegBaseClassCode, base);
-    uint16_t command;
-    _pci.ReadPciReg(DevPci::kCommandReg, command);
-    command |= DevPci::kCommandRegBusMasterEnableFlag;
-    _pci.WritePciReg(DevPci::kCommandReg, command);
-    */
-  // mmap BAR0
+  MapControlRegisters();
 
-  volatile uint8_t *controlRegisters8BaseAddr = nullptr;
-  volatile uint32_t *controlRegisters32BaseAddr = nullptr;
-  uint8_t controlRegisters8Size = 0;
   {
-    uint32_t addr_bkup;
-    _pci.ReadPciReg(DevPci::kBaseAddressReg0, addr_bkup);
-    _pci.WritePciReg(DevPci::kBaseAddressReg0, 0xFFFFFFFF);
-    uint32_t size;
-    _pci.ReadPciReg(DevPci::kBaseAddressReg0, size);
-    size = ~size + 1;
-    _pci.WritePciReg(DevPci::kBaseAddressReg0, addr_bkup);
-
-    int fd = open("/sys/class/uio/uio0/device/resource0", O_RDWR);
-    controlRegisters8BaseAddr = reinterpret_cast<volatile uint8_t *>(
-        mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
-    controlRegisters8Size = size;
-    close(fd);
+    // check CAP
+    ControllerCapabilities cap;
+    cap.qword = _ctrl_reg_64_base[kCtrlReg64OffsetCAP];
+    PrintControllerCapabilities(cap);
+    _ctrl_timeout_worst = cap.bits.TO * 500 * 1000;  // in unit 500ms -> us
   }
-  printf("BAR0: %p %d\n", controlRegisters8BaseAddr, controlRegisters8Size);
-  controlRegisters32BaseAddr =
-      reinterpret_cast<volatile uint32_t *>(controlRegisters8BaseAddr);
 
-  printf("CSTS: 0x%08X\n",
-         controlRegisters32BaseAddr[kControlRegister32OffsetCSTS]);
+  {
+    // reset if needed
+    ControllerStatus csts;
+    csts.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCSTS];
+    PrintControllerStatus(csts);
+    if (csts.bits.RDY) {
+      puts("Performing reset...");
+      ControllerConfiguration cc;
+      cc.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCC];
+      cc.bits.EN = 0;
+      _ctrl_reg_32_base[kCtrlReg32OffsetCC] = cc.dword;
+      //
+      usleep(_ctrl_timeout);
+      csts.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCSTS];
+      if (csts.bits.RDY) {
+        // try again
+        usleep(_ctrl_timeout);
+        csts.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCSTS];
+        if (csts.bits.RDY) {
+          puts("Failed to reset controller.");
+          exit(EXIT_FAILURE);
+        }
+      }
+    }
+  }
+
+  InitAdminQueues();
+
+  {
+    // print current status
+    ControllerConfiguration cc;
+    ControllerStatus csts;
+    cc.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCC];
+    PrintControllerConfiguration(cc);
+    csts.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCSTS];
+    PrintControllerStatus(csts);
+  }
+
+  {
+    // set ControllerConfiguration
+    ControllerConfiguration cc;
+    cc.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCC];
+    // PrintControllerConfiguration(cc);
+    cc.bits.AMS = kCC_AMS_RoundRobin;
+    cc.bits.MPS = 0;  // for 4KB. TODO: changed to be decided dynamically.
+    cc.bits.CSS = kCC_CSS_NVMeCommandSet;
+    _ctrl_reg_32_base[kCtrlReg32OffsetCC] = cc.dword;
+  }
+
+  {
+    // enable controller
+    ControllerConfiguration cc;
+    ControllerStatus csts;
+
+    puts("Enabling controller...");
+    cc.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCC];
+    cc.bits.EN = 1;
+    _ctrl_reg_32_base[kCtrlReg32OffsetCC] = cc.dword;
+    //
+    usleep(_ctrl_timeout);
+    csts.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCSTS];
+    if (!csts.bits.RDY) {
+      // try again
+      usleep(_ctrl_timeout);
+      csts.dword = _ctrl_reg_32_base[kCtrlReg32OffsetCSTS];
+      if (!csts.bits.RDY) {
+        puts("Failed to enable controller.");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 
   return;
 }
--- nvme.h
+++ nvme.h
@@ -14,6 +14,75 @@
 
 class DevNvme {
  public:
+  struct CommandSet {
+    uint32_t CDW0;
+    uint32_t NSID;
+    uint64_t Reserved0;
+    uint64_t MPTR;
+    uint64_t PRP1;
+    uint64_t PRP2;
+    uint32_t CDW10;
+    uint32_t CDW11;
+    uint32_t CDW12;
+    uint32_t CDW13;
+    uint32_t CDW14;
+    uint32_t CDW15;
+  };
+  struct CompletionQueueEntry {
+    uint32_t DW0;
+    uint32_t DW1;
+    uint16_t SQHD;
+    uint16_t SQID;
+    uint16_t SF;  // includes Phase Tag
+    uint16_t CID;
+  } __attribute__((packed));
+
+  union ControllerCapabilities {
+    uint64_t qword;
+    struct {
+      unsigned MQES : 16;
+      unsigned CQR : 1;
+      unsigned AMS : 2;
+      unsigned Reserved0 : 5;
+      unsigned TO : 8;  // in 500ms unit
+      unsigned DSTRD : 4;
+      unsigned NSSRS : 1;
+      unsigned CSS : 8;
+      unsigned BPS : 1;
+      unsigned Reserved1 : 2;
+      unsigned MPSMIN : 4;
+      unsigned MPSMAX : 4;
+      unsigned Reserved2 : 8;
+    } bits;
+  };
+
+  union ControllerConfiguration {
+    uint32_t dword;
+    struct {
+      unsigned EN : 1;
+      unsigned Reserved0 : 3;
+      unsigned CSS : 3;
+      unsigned MPS : 4;
+      unsigned AMS : 3;
+      unsigned SHN : 2;
+      unsigned IOSQES : 4;
+      unsigned IOCQES : 4;
+      unsigned Reserved1 : 8;
+    } bits;
+  };
+
+  union ControllerStatus {
+    uint32_t dword;
+    struct {
+      unsigned RDY : 1;
+      unsigned CFS : 1;
+      unsigned SHST : 2;
+      unsigned NSSRO : 1;
+      unsigned PP : 1;
+      unsigned Reserved : 28;
+    } bits;
+  };
+
   void Init();
   /*
   void Run() {
@@ -31,6 +100,32 @@ class DevNvme {
   }
   */
  private:
-  static const int kControlRegister32OffsetCSTS = 0x1C / sizeof(uint32_t);
   DevPci _pci;
+
+  static const int kCtrlReg64OffsetCAP = 0x00 / sizeof(uint64_t);
+  static const int kCtrlReg32OffsetCC = 0x14 / sizeof(uint32_t);
+  static const int kCtrlReg32OffsetCSTS = 0x1C / sizeof(uint32_t);
+  static const int kCtrlReg32OffsetAQA = 0x24 / sizeof(uint32_t);
+  static const int kCtrlReg64OffsetASQ = 0x28 / sizeof(uint64_t);
+  static const int kCtrlReg64OffsetACQ = 0x30 / sizeof(uint64_t);
+
+  static const int kASQSize = 8;
+  static const int kACQSize = 8;
+
+  static const int kCC_AMS_RoundRobin = 0b000;
+  static const int kCC_CSS_NVMeCommandSet = 0b000;
+
+  volatile uint8_t *_ctrl_reg_8_base = nullptr;
+  uint8_t _ctrl_reg_8_size = 0;
+  volatile uint32_t *_ctrl_reg_32_base = nullptr;
+  volatile uint64_t *_ctrl_reg_64_base = nullptr;
+
+  __useconds_t _ctrl_timeout_worst = 0;  // set in Init()
+  static const __useconds_t _ctrl_timeout = 500 * 1000;
+
+  Memory *_mem_for_asq;
+  Memory *_mem_for_acq;
+
+  void MapControlRegisters();
+  void InitAdminQueues();
 };
